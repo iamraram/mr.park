d5a7f5f3a41e4b6d6da213ed8655b7db3421e448
iamraram <slyram06@icloud.com>
2022-08-30 23:17:59 +0900

    first commit

---
+++ app/src/main/java/com/haram/mrpark/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.haram.mrpark
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
