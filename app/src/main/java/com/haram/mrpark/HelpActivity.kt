9c957e957e3b9899debde9d51ced8dd5fd182eff
Heechan <ckstmznf@naver.com>
2022-09-07 00:53:14 +0900

    Help Activity

--- app/src/main/java/com/haram/mrpark/HelpActivity.kt
+++ app/src/main/java/com/haram/mrpark/HelpActivity.kt
@@ -2,10 +2,15 @@ package com.haram.mrpark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+
+        findViewById<Button>(R.id.btn_finish).setOnClickListener{
+            finish()
+        }
     }
 }
--- app/src/main/java/com/haram/mrpark/MainActivity.kt
+++ app/src/main/java/com/haram/mrpark/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.haram.mrpark
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +18,13 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, PlayActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        val helpBtn = findViewById<TextView>(R.id.txt_main_help)
+
+        helpBtn.setOnClickListener{
+            val intent = Intent(this, HelpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
