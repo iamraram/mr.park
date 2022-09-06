66ddac529ee19f972824bcbb5c9609f65ecc9557
Heechan <ckstmznf@naver.com>
2022-09-07 00:02:45 +0900

    Data add

--- app/src/main/java/com/haram/mrpark/gameData.kt
+++ app/src/main/java/com/haram/mrpark/gameData.kt
@@ -9,49 +9,66 @@ val GAME_DATA = mutableListOf(
     Heechan(
         "체육관에 다니기 시작했다. 벌써부터 포스가 느껴진다.",
         R.drawable.lv2,
-        96,
+        85,
     ),
     Heechan(
         "벌써부터 무술로 사람 한 명을 제압할 수 있다.",
         R.drawable.lv3,
-        92,
+        70,
     ),
     Heechan(
         "꽃과 함께하는 아름다운 자태가 꽤나 매력적이다.",
         R.drawable.lv4,
-        88,
-    ),
-    Heechan(
-        "꽃과 함께하는 아름다운 자태가 꽤나 매력적이다.",
-        R.drawable.lv5,
-        84,
+        55,
     ),
     Heechan(
         "1988 올림픽을 응원하는 캡틴 코리아 박희찬이다.",
-        R.drawable.lv6,
-        80,
+        R.drawable.lv5,
+        50,
     ),
     Heechan(
         "부끄뎌운 희딴이 ver 1 - 매우 수줍어한다.",
-        R.drawable.lv7,
-        75,
+        R.drawable.lv6,
+        45,
     ),
     Heechan(
         "부끄뎌운 희딴이 ver 2 - 깜찍한 포즈의 가운뎃손가락.",
-        R.drawable.lv8,
-        70,
+        R.drawable.lv7,
+        40,
     ),
     Heechan(
         "부끄뎌운 희딴이 ver 3 - 새빨개진 얼굴로 입을 가린다.",
-        R.drawable.lv9,
-        65,
-    ),Heechan(
+        R.drawable.lv8,
+        35,
+    ),
+    Heechan(
         "선린 입학 첫 날 미연시를 하는 희찬.",
-        R.drawable.lv10,
-        60,
-    ),Heechan(
+        R.drawable.lv9,
+        30,
+    ),
+    Heechan(
         "애플파이에 들어가기를 희망하는 신입생 희찬.",
         R.drawable.lv10,
-        55,
+        25,
+    ),
+    Heechan(
+        "애플파이 면접을 광탈하고 우는 그의 모습…",
+        R.drawable.lv11,
+        15,
+    ),
+    Heechan(
+        "애플파이 미련을 못 버려 빨간 옷을 찾아 입는다.",
+        R.drawable.lv12,
+        5,
+    ),
+    Heechan(
+        "그리고 그는 결국 학교에서…",
+        R.drawable.lv13,
+        2,
+    ),
+    Heechan(
+        "어쩔 수 없이 눈물겹게 에드캔에 입부하고 만다.",
+        R.drawable.lv14,
+        1,
     ),
 )
